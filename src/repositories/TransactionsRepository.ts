8d2afdbcd6879fd213ad1501344ebcd3578ffa11
Elton Mozart <elton.mozart@gmail.com>
2020-04-26 19:01:35 -0300

    Bootcamp GosStack: Desafio 4 - NodeJS com Typescript
    Desafio resolvido.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, cur) => {
+      if (cur.type === 'income') {
+        total += cur.value;
+      }
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, cur) => {
+      if (cur.type === 'outcome') {
+        total += cur.value;
+      }
+      return total;
+    }, 0);
+
+    const res: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+    return res;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const nt = new Transaction({ title, value, type });
+    this.transactions.push(nt);
+    return nt;
   }
 }
 
